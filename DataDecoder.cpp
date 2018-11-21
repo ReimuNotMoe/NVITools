131343ff11d7b4605e4312ba08b4c392e63fd8fb
ReimuNotMoe <34613827+ReimuNotMoe@users.noreply.github.com>
2018-11-21 12:04:13 +0800

    nvi json rpc data decoder

---
+++ DataDecoder.cpp
@@ -0,0 +1,46 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cinttypes>
+#include <vector>
+
+int main(int argc, char **argv) {
+
+	std::vector<uint8_t> buf;
+
+	for (int i=1; i<argc; i++) {
+		auto byte = (uint8_t)strtol(argv[i], NULL, 10);
+		buf.push_back(byte);
+	}
+
+	printf("Length: %zu\n", buf.size());
+
+//	printf("Data: ");
+//
+//	for (auto &it : buf) {
+//		printf("%" PRIu8 " ", it);
+//	}
+//
+//	printf("\n\n");
+
+	if (buf.size() == 4) {
+		printf("x32: %" PRIx32 "\n", *(uint32_t *)buf.data());
+		printf("u32: %" PRIu32 "\n", *(uint32_t *)buf.data());
+		printf("d32: %" PRId32 "\n", *(uint32_t *)buf.data());
+
+		printf("\n\n");
+	}
+
+	if (buf.size() == 8) {
+		printf("x64: %" PRIx64 "\n", *(uint64_t *)buf.data());
+		printf("u64: %" PRIu64 "\n", *(uint64_t *)buf.data());
+		printf("d64: %" PRId64 "\n", *(uint64_t *)buf.data());
+
+		printf("\n\n");
+	}
+
+	buf.push_back(0);
+	printf("String: %s\n", buf.data());
+
+	return 0;
+
+}
